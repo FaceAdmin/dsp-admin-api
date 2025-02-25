06cdd6f18df66880822ac136ff5b3cbe80f19571
akusvvz <116110183+baktygaliyevv@users.noreply.github.com>
2025-02-25 17:26:13 +0000

    fixes for users page

--- apps/users/models.py
+++ apps/users/models.py
@@ -11,11 +11,6 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def save(self, *args, **kwargs):
-        if not self.pk or ('password' in kwargs.get('update_fields', []) and is_password_usable(self.password)):
-            self.password = make_password(self.password)
-        super(User, self).save(*args, **kwargs)
-
     class Meta:
         db_table = 'users'
 
--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 from .models import User, Photo
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
+    newPassword = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = ['user_id', 'fname', 'lname', 'email', 'role', 'password', 'created_at', 'updated_at']
+        fields = ['user_id', 'fname', 'lname', 'email', 'role', 'password', 'newPassword', 'created_at', 'updated_at']
         extra_kwargs = {
             'password': {'write_only': True},
         }
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        newPassword = validated_data.pop('newPassword', None)
+        if newPassword:
+            instance.password = make_password(newPassword)
+        return super().update(instance, validated_data)
+
 
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
